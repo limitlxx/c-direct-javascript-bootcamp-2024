ec2f586b61466c2b7aff5110f2de9f1a40254e97
limitlxx <elgravicodesh>
2024-10-31 12:20:59 +0100

    Functions

--- script/index.js
+++ script/index.js
@@ -68,4 +68,23 @@
 **/
 
 
+// Functions, Conditionals, Datatypes, Events
+function Addition() {
+    let x = parseFloat(document.getElementById("params1").value);
+    let y = parseFloat(document.getElementById("params2").value);
+
+    console.log(`x: ${x}, y: ${y}`);    
+
+    if(x == "" || y == ""){
+        alert("Enter a valye");
+        return false;
+    } else {
+        
+        z = x + y;
+        console.log(`z: ${z}`);
+        document.getElementById("solution").value = z;
+    }
+}
+
+
     
